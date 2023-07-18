8c01437bc4aa8e0c5ae557e0d19789e85bb5d397
logicHoon <dlclgns152615@gmail.com>
2023-07-18 11:06:20 +0900

    [Feat] 사용자가 특정 척도를 넘어서면 척도 한계치로 복귀시키는 기능 추가

--- Trash_Where_iOS/Feature/Map/MapViewController.swift
+++ Trash_Where_iOS/Feature/Map/MapViewController.swift
@@ -11,13 +11,26 @@ import RxSwift
 import SnapKit
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController{
 
   // MARK: - Properties
   
+  // UI Properties
   var mapView: MKMapView!
   var locationManager = CLLocationManager()
   var userLocationButton = UIButton()
+  let userLocationButtonImageView = UIImageView(image: UIImage(named: "GPSemoji"))
+  
+  // Feature Properties
+//  lazy var center = mapView.centerCoordinate
+//  lazy var span = mapView.region.span
+//
+//  let maxLatitude: CLLocationDegrees = 42.0
+//  let minLatitude: CLLocationDegrees = 29.0
+//  let maxLongitude: CLLocationDegrees = 130.0
+//  let minLongitude: CLLocationDegrees = 125.0
+//
+//  lazy var newCenter = center
   
   // MARK: - LifeCycle
   
@@ -27,18 +40,17 @@ class MapViewController: UIViewController {
     
     configureSubviews()
   }
+  
 
 // MARK: - Action
   
-  
+ 
   
 // MARK: - UISetting
   
   func setupUserLocationButton() {
-    userLocationButton.setImage(UIImage(named: "GPSemoji"), for: .normal)
     userLocationButton.backgroundColor = .white
     userLocationButton.layer.cornerRadius = 20
-    userLocationButton.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
     userLocationButton.layer.shadowColor = UIColor.black.cgColor
     userLocationButton.layer.shadowOffset = CGSize(width: 0, height: 2)
     userLocationButton.layer.shadowOpacity = 0.5
@@ -55,14 +67,36 @@ extension MapViewController: MKMapViewDelegate {
   func setupMapView() {
     mapView.delegate = self
     mapView.showsUserLocation = true // 사용자 위치
-  }
-  
-  func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-    let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.004, longitudeDelta: 0.004))
     
-    mapView.setRegion(region, animated: true)
   }
   
+  // 척도 범위 설정
+  func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+    let center = mapView.userLocation.coordinate
+    let zoomLevel = log2(360 * ((Double(mapView.frame.size.width/256)) / mapView.region.span.longitudeDelta))
+    if zoomLevel < 10 {
+      let span = MKCoordinateSpan(latitudeDelta: 0, longitudeDelta: 360/pow(2, 10)*Double(mapView.frame.size.width/256))
+      let region = MKCoordinateRegion(center: center, span: span)
+      mapView.setRegion(region, animated: true)
+    }
+  }
+  
+  // 지도의 범위
+//  if center.latitude > maxLatitude {
+//    newCenter.latitude = maxLatitude - span.latitudeDelta / 2
+//  } else if center.latitude < minLatitude {
+//    newCenter.latitude = minLatitude + span.latitudeDelta / 2
+//  }
+//
+//  if center.longitude > maxLongitude {
+//    newCenter.longitude = maxLongitude - span.longitudeDelta / 2
+//  } else if center.longitude < minLongitude {
+//    newCenter.longitude = minLongitude + span.longitudeDelta / 2
+//  }
+//
+//  if newCenter.latitude != center.latitude || newCenter.longitude != center.longitude {
+//    mapView.setRegion(MKCoordinateRegion(center: newCenter, span: span), animated: true)
+//  }
 }
 
 // MARK: - CLLocationManagerDelegate
@@ -79,6 +113,16 @@ extension MapViewController: CLLocationManagerDelegate {
     locationManager.startUpdatingLocation()
   }
   
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let location = locations.last else { return }
+    
+    let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+    mapView.setRegion(region, animated: true)
+    
+    // 위치 업데이트 종료
+    locationManager.stopUpdatingLocation()
+  }
+  
 }
 
 // MARK: - LayoutSupport Protocol
@@ -93,6 +137,8 @@ extension MapViewController: LayoutSupport {
     self.view.addSubview(mapView)
     
     mapView.addSubview(userLocationButton)
+    
+    userLocationButton.addSubview(userLocationButtonImageView)
   }
   
   func setupLayouts() {
@@ -105,8 +151,10 @@ extension MapViewController: LayoutSupport {
 extension MapViewController: SetupSubviewsLayouts {
   
   func setupSubviewsLayouts() {
-    setupMapView()
     setupCLLocationManager()
+    setupMapView()
+    
+    // UISetup
     setupUserLocationButton()
   }
   
@@ -120,6 +168,13 @@ extension MapViewController: SetupSubviewsConstraints {
       $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(35)
       $0.height.equalTo(40)
       $0.width.equalTo(40)
+    }
+    
+    userLocationButtonImageView.snp.makeConstraints {
+      $0.top.equalTo(userLocationButton.snp.top).offset(8)
+      $0.leading.equalTo(userLocationButton.snp.leading).offset(8)
+      $0.trailing.equalTo(userLocationButton.snp.trailing).inset(8)
+      $0.bottom.equalTo(userLocationButton.snp.bottom).inset(8)
     }
   }
 
